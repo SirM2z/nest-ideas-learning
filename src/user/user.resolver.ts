dd79a0d93022933f4737a2cef6a9fa41bc75e052
sirm2z <sirm2zisv@gmail.com>
2019-04-17 17:48:38 +0800

    Added GraphQL queries

--- src/user/user.resolver.ts
+++ src/user/user.resolver.ts
@@ -1,9 +1,28 @@
-import { Resolver, Query } from '@nestjs/graphql';
+import {
+  Resolver,
+  Query,
+  Args,
+  Parent,
+  ResolveProperty,
+} from '@nestjs/graphql';
+import { UserService } from './user.service';
+import { CommentService } from '../comment/comment.service';
 
 @Resolver('User')
 export class UserResolver {
+  constructor(
+    private userService: UserService,
+    private commentService: CommentService,
+  ) {}
+
   @Query()
-  users() {
-    return [{ id: 'id', username: 'username' }];
+  users(@Args('page') page: number, @Args('pageSize') pageSize: number) {
+    return this.userService.showAll(page, pageSize);
+  }
+
+  @ResolveProperty()
+  comments(@Parent() user) {
+    const { id } = user;
+    return this.commentService.showByUser(id);
   }
 }
